ed86b54f928e214f3437c7de3ae972d9efa3a5d1
Diego Lima <diegolimareis021@gmail.com>
2026-09-18 16:56:35 -0300

    feat: add HospitalTicket class for patient queue management

---
+++ FilaMed/Model/HospitalTicket.cs
@@ -0,0 +1,21 @@
+﻿
+namespace FilaMed.Model
+{
+    internal class HospitalTicket
+    {
+        private string Name {  get; set; } = string.Empty;
+        private DateTime IssuedAt { get; set; }
+
+        public HospitalTicket(string name, DateTime issuedAt)
+        {
+            Name = name;
+            IssuedAt = issuedAt;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Name} | {IssuedAt:HH:MM} ";
+        }
+    }
+}
