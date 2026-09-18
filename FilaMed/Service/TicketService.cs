6467427917076f815577b0d1c932ad5d3ba3f33b
Diego Lima <diegolimareis021@gmail.com>
2026-09-18 16:58:31 -0300

    feat: add TicketService to generate sequential hospital tickets

---
+++ FilaMed/Service/TicketService.cs
@@ -0,0 +1,42 @@
+﻿
+using FilaMed.Model;
+
+namespace FilaMed.Service
+{
+    internal class TicketService
+    {
+        private static int Count = 0;
+
+
+        public static HospitalTicket GeneratePassword()
+        {
+            Count++;
+
+            int remainder = Count % 3;
+
+            string number = Count.ToString("D2");
+            string letter = string.Empty;
+
+
+            if (remainder == 1)
+            {
+                letter = "A";
+            }
+            else if (remainder == 2)
+            {
+
+                letter = "B";
+            }
+            else if (remainder == 3)
+            {
+                letter = "C";
+            }
+
+            string format = letter + number;
+
+            var now = DateTime.Now;
+
+            return new HospitalTicket(format, now);
+        }
+    }
+}
